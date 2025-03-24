66c9ea2bfc871428e2a4c2ba674959ce62f58a13
nabilulilalbab <nabilulilalbab78@gmail.com>
2025-03-25 04:45:42 +0700

    day 2

--- template_action_test.go
+++ template_action_test.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -41,3 +42,89 @@ func TestTemplateComparator(t *testing.T)  {
   body, _ := io.ReadAll(recorder.Result().Body)
   fmt.Println(string(body))
 }
+
+func ToUpper(s string) string {
+  return strings.ToUpper(s)
+}
+
+
+func Discount(price any, percent any) string {
+	priceFloat, _ := price.(float64)
+	percentFloat, _ := percent.(float64)
+
+	discounted := priceFloat - (priceFloat * percentFloat / 100)
+	return fmt.Sprintf("%.2f", discounted)
+}
+
+func TemplateActionCustomFunction(w http.ResponseWriter, r *http.Request)  {
+  funcMap := template.FuncMap{
+    "ToUpper" : ToUpper,
+    "Discount" : Discount,
+  }
+  t := template.Must(template.New("customFunction.html").Funcs(funcMap).ParseFiles("./templates/customFunction.html"))
+  t.ExecuteTemplate(w, "customFunction.html", map[string]any{
+    "Name" : "nabiel",
+    "Price":    float64(100000), // Ubah ke float64
+    "Discount": float64(20),     // Ubah ke float64
+  })
+}
+
+func TestTemplateCustomFunction(t *testing.T)  {
+  request := httptest.NewRequest(http.MethodGet, "http://localhost", nil)
+  recorder := httptest.NewRecorder()
+  TemplateActionCustomFunction(recorder, request)
+  body, _ := io.ReadAll(recorder.Result().Body)
+  fmt.Println(string(body))
+}
+
+func TemplateActionRange(w http.ResponseWriter, r *http.Request)  {
+  t := template.Must(template.ParseFiles("./templates/range.html"))
+  context := map[string]any{
+    "Title" : "Template Action Range",
+    "Hobbies" : []string{
+      "Gaming", "Reading", "Coding",
+    },
+  } 
+  t.ExecuteTemplate(w, "range.html", context)
+}
+
+func TestTemplateActionRange(t *testing.T)  {
+  request := httptest.NewRequest(http.MethodGet, "http://localhost", nil)
+  recorder := httptest.NewRecorder()
+  TemplateActionRange(recorder, request)
+  body , _ := io.ReadAll(recorder.Result().Body)
+  fmt.Println(string(body))
+}
+
+type PageCheckout struct{
+  Title string
+  Name string
+  Address2
+}
+
+type Address2 struct {
+  City string
+  Street string
+}
+
+func TemplateActionWith(w http.ResponseWriter, r *http.Request)  {
+  t := template.Must(template.ParseFiles("./templates/with.html"))
+  context := PageCheckout{
+    Title: "CheckOut",
+    Name: "Nabiel",
+    Address2: Address2{
+      City: "Demak",
+      Street: "Jl. Demung kerangkulon, wonosalam, Demak",
+    },
+  } 
+  t.ExecuteTemplate(w, "with.html", context)
+}
+
+func TestTemplateActionWith(t *testing.T)  {
+  request := httptest.NewRequest(http.MethodGet, "http://localhost", nil)
+  recorder := httptest.NewRecorder()
+  TemplateActionWith(recorder, request)
+  body , _ := io.ReadAll(recorder.Result().Body)
+  fmt.Println(string(body))
+}
+
